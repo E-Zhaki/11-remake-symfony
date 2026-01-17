71c71a87d56221aadb5b8be7b574e2554bebe569
El ouarroudi <zhaki.elouarroudi@gmail.com>
2026-01-17 01:18:11 +0100

    Début CRUD

--- migrations/Version20260116225235.php
+++ migrations/Version20260116225235.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260116212724 extends AbstractMigration
+final class Version20260116225235 extends AbstractMigration
 {
     public function getDescription(): string
     {
